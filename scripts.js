b6509077ddcc7e0384cb948fc7b031fdfd13ea06
Michael Garcia <agency@ilovemycrm.com>
2024-07-13 17:06:20 -0700

    remove modal function
    makeing a success page vs loading a modal on the event page.

--- scripts.js
+++ scripts.js
@@ -4,9 +4,6 @@
     const currentURL = window.location
 
     loadFathomAnalytics()
-    if (currentURL.href === 'https://iebarnabas.org/q3-meeting') {
-        updateRegistrationSection()
-    }
 
     function loadFathomAnalytics() {
         const fathomScript = document.createElement('script')
@@ -14,39 +11,5 @@
         fathomScript.setAttribute('data-site', 'KWZSKWJS')
         fathomScript.defer = true
         document.head.appendChild(fathomScript)
-    }
-
-    function updateRegistrationSection() {
-        // Get the URL parameters
-        const params = new URLSearchParams(currentURL.search)
-
-        // Check if the 'fname' parameter is present and not empty
-        if (params.has('fname')) {
-            // Get the value of the 'fname' parameter
-            const fname = params.get('fname')
-
-            // Ensure fname is not empty
-            if (fname.trim() !== '') {
-                // Find the div with the class 'iebg-registration-section'
-                const registrationSection = document.querySelector(
-                    '.iebg-registration-section'
-                )
-                if (registrationSection) {
-                    // Add the 'iebg-registration-section--visible' class to the registration section
-                    registrationSection.classList.add(
-                        'iebg-registration-section--visible'
-                    )
-                }
-
-                // Find the div with the class 'iebg-registration-section--guest-name'
-                const guestNameSection = document.querySelector(
-                    '.iebg-registration-section--guest-name'
-                )
-                if (guestNameSection) {
-                    // Replace the text content with the value of the 'fname' parameter
-                    guestNameSection.textContent = fname
-                }
-            }
-        }
     }
 })()
