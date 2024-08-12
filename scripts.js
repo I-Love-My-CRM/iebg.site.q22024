e6a223ca48bc7e444d661b53110ec63bbaf8182c
Michael Garcia <agency@ilovemycrm.com>
2024-08-12 15:32:53 -0700

    update fathom analtyics

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@
     function loadFathomAnalytics() {
         const fathomScript = document.createElement('script')
         fathomScript.src = 'https://cdn.usefathom.com/script.js'
-        fathomScript.setAttribute('data-site', 'KWZSKWJS')
+        fathomScript.setAttribute('data-site', 'DTEWFXQW')
         fathomScript.defer = true
         document.head.appendChild(fathomScript)
     }
